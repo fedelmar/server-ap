ba3fd30ed7d357976710c6f3b7d6a46b3645dc3f
Federico Suarez <fsuarez91@gmail.com>
2020-07-16 09:28:52 -0300

    Obtener cliente por ID

--- db/resolvers.js
+++ db/resolvers.js
@@ -64,6 +64,21 @@ const resolvers = {
             return insumo;
         },
 
+        obtenerCliente: async (_, {id}, ctx ) => {
+            //Verificar existencia
+            const cliente = await Cliente.findById(id);
+
+            if (!cliente) {
+                throw new Error('Cliente no encontrado');
+            }
+
+            if(cliente.vendedor.toString() !== ctx.usuario.id ) {
+                throw new Error('No tienes las credenciales.');
+            } 
+
+            return cliente;
+        },
+
         obtenerClientes: async () => {
             try {
                 const clientes = await Cliente.find({});
--- db/schema.js
+++ db/schema.js
@@ -87,6 +87,7 @@ const typeDefs = gql`
         # Clientes
         obtenerClientes: [Cliente]
         obtenerClientesVendedor: [Cliente]
+        obtenerCliente(id: ID!): Cliente
     }
 
     type Mutation {
