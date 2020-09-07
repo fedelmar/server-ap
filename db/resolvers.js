6f0cad83fe19ec9663499eb5424c281247955f87
fedelmar <fsuarez91@gmail.com>
2020-09-07 11:26:36 -0300

    Fix actualizarInsumos

--- db/resolvers.js
+++ db/resolvers.js
@@ -567,8 +567,11 @@ const resolvers = {
             if (!insumo) {
                 throw new Error('No existe el insumo en stock')
             }
-
-            insumo = await StockInsumo.findByIdAndUpdate( {_id: id}, input, { new: true });
+            if (input.cantidad !== 0) {
+                insumo = await StockInsumo.findByIdAndUpdate( {_id: id}, input, { new: true });
+            } else {
+                insumo = await StockInsumo.findByIdAndDelete({_id: id})
+            }          
             
             return insumo;            
         },
