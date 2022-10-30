419c7288f5ae4995b322a908574961ad158e47ec
Federico Suarez <fsuarez@intermediait.com>
2022-10-30 20:50:52 -0300

    fix html option

--- crons/sendEmail.js
+++ crons/sendEmail.js
@@ -1,13 +1,13 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer')
 
 const sendEmail = (insumosFaltantes, subject) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: 'gmail',
     auth: {
-      user: "martin.mdp92@gmail.com",
-      pass: process.env.EMAIL_KEY,
-    },
-  });
+      user: 'martin.mdp92@gmail.com',
+      pass: process.env.EMAIL_KEY
+    }
+  })
 
   const mailContent = () => {
     const thead = `
@@ -17,48 +17,48 @@ const sendEmail = (insumosFaltantes, subject) => {
           <th>Categoria</th>
           <th>Cantidad</th>
         </tr>
-      </thead>`;
+      </thead>`
 
-    let insumos = '';
+    let insumos = ''
     insumosFaltantes.forEach((insumo) => {
       insumos += `
         <tr>
-        <td> ${insumo.insumo} </td>
-        <td> ${insumo.categoria} </td>
-        <td> ${insumo.cantidad} </td>
-        </tr>`;
-    });
+          <td> ${insumo.insumo} </td>
+          <td> ${insumo.categoria} </td>
+          <td> ${insumo.cantidad} </td>
+        </tr>`
+    })
 
     const tbody = `
       <tbody>   
       ${insumos}
-      </tbody>`;
+      </tbody>`
 
-    return (`
+    return `
       <html>
-        <table cellspacing="0" cellpadding="0" width="640" align="center" border="1">
+        <h3 align="start">Insumos con Faltante</h3>
+        <table cellspacing="0" cellpadding="0" width="640" border="1">
           ${thead}
           ${tbody}
         </table>
       </html>`
-    );
+  }
+
+  const mailOptions = {
+    from: 'martin.mdp92@gmail.com',
+    to: ['fsuarez91@gmail.com'],
+    subject: subject,
+    html: mailContent()
+  }
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Se envio el email: ' + subject)
+      console.log('Email sent: ' + info.response)
+    }
+  })
 }
 
-const mailOptions = {
-  from: "martin.mdp92@gmail.com",
-  to: ["martinrdilorenzo@gmail.com"],
-  subject: subject,
-  text: mailContent(),
-};
-
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Se envio el email: " + subject);
-    console.log("Email sent: " + info.response);
-  }
-});
-};
-
-module.exports = { sendEmail };
+module.exports = { sendEmail }
