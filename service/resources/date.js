f46ceb29225f832ba6b9fcdfaafa149a9ec265c1
farpires <arpirez.fabio@gmail.com>
2023-04-21 11:31:51 -0300

    Added a new function to generate a date with the format (DD/MM/YYYY)

---
+++ service/resources/date.js
@@ -0,0 +1,7 @@
+function format(date) {
+	const pad = (n) => (n < 10 ? `0${n}` : n);
+	return `${pad(date.getDate())}/${pad(date.getMonth())}/${pad(date.getFullYear())}`;
+}
+
+
+module.exports={format}
