c8dcb2db62a263894200eb5fba8f2ae6f23a9434
Federico Suarez <fsuarez@intermediait.com>
2023-05-23 23:07:02 -0300

    fix cron

--- service/job.js
+++ service/job.js
@@ -5,11 +5,10 @@ let tarea
 
 const job = async () => {
   const regla = new schedule.RecurrenceRule()
-  // regla.month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-  // regla.dayOfWeek = [0, 1, 2, 3, 4, 5, 6]
-  // regla.hour = 22
-  // regla.minute = 45
-  regla.second = 0
+  regla.month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  regla.dayOfWeek = 1
+  regla.hour = 9
+  regla.minute = 30
 
   tarea = schedule.scheduleJob(regla, function () {
     console.log('Ejecutando tarea el primer día de cada mes a las 9:30 AM')
