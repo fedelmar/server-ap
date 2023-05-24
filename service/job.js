e294b3a1ce9f3e4a3631adb273b2ca0f0e7158aa
Federico Suarez <fsuarez@intermediait.com>
2023-05-23 23:12:11 -0300

    test staging cron

--- service/job.js
+++ service/job.js
@@ -5,10 +5,11 @@ let tarea
 
 const job = async () => {
   const regla = new schedule.RecurrenceRule()
-  regla.month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-  regla.dayOfWeek = 1
-  regla.hour = 9
-  regla.minute = 30
+  // regla.month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  // regla.dayOfWeek = 1
+  // regla.hour = 9
+  // regla.minute = 30
+  regla.second = 0
 
   tarea = schedule.scheduleJob(regla, function () {
     console.log('Ejecutando tarea el primer día de cada mes a las 9:30 AM')
