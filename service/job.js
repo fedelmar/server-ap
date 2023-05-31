67d6e37f0b261f7525ba8819c2ebe959bd330f34
Federico Suarez <fsuarez@intermediait.com>
2023-05-31 08:30:18 -0300

    test

--- service/job.js
+++ service/job.js
@@ -8,7 +8,7 @@ const job = async () => {
   // regla.month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   // regla.dayOfWeek = 1
   // regla.hour = 9
-  regla.minute = 1
+  regla.second = 1
 
   tarea = schedule.scheduleJob(regla, function () {
     console.log('Ejecutando tarea el primer día de cada mes a las 9:30 AM')
