a485ea74000dd26595514e535f5e1bb72cc65a9b
Federico Suarez <fsuarez@intermediait.com>
2023-05-31 17:14:01 -0300

    set schedule

--- service/job.js
+++ service/job.js
@@ -5,11 +5,10 @@ let tarea
 
 const job = async () => {
   const regla = new schedule.RecurrenceRule()
-  // regla.month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-  // regla.dayOfWeek = 1
-  // regla.hour = 9
-  // regla.minute = 30
-  regla.second = 1
+  regla.month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  regla.dayOfWeek = 1
+  regla.hour = 9
+  regla.minute = 30
 
   tarea = schedule.scheduleJob(regla, function () {
     console.log('Ejecutando tarea el primer día de cada mes a las 9:30 AM')
