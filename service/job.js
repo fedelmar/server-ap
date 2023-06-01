ee5d97cd0b6c0f32ea0974b21887e35ba79e4181
Federico Suarez <fsuarez@intermediait.com>
2023-06-01 13:17:06 -0300

    update cron rule

--- service/job.js
+++ service/job.js
@@ -6,9 +6,21 @@ let tarea
 const job = async () => {
   const regla = new schedule.RecurrenceRule()
   regla.month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-  regla.dayOfWeek = 1
   regla.hour = 9
   regla.minute = 30
+  regla.second = 0
+
+  regla.date = 1
+  regla.dayOfWeek = [new schedule.Range(1, 5)] // Lunes a Viernes
+
+  const fechaActual = new Date()
+  const diaActual = fechaActual.getDate()
+  const diaSemanaActual = fechaActual.getDay()
+
+  if (diaActual === 1 && (diaSemanaActual === 0 || diaSemanaActual === 6)) {
+    // Si el primer día del mes es sábado (6) o domingo (0), se programa para el siguiente lunes (1).
+    regla.date = 2
+  }
 
   tarea = schedule.scheduleJob(regla, function () {
     console.log('Ejecutando tarea el primer día de cada mes a las 9:30 AM')
