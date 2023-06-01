9131396d228ff4c786c26319674c2698065f609a
Federico Suarez <fsuarez@intermediait.com>
2023-06-01 13:19:19 -0300

    change hour for testing

--- service/job.js
+++ service/job.js
@@ -6,8 +6,8 @@ let tarea
 const job = async () => {
   const regla = new schedule.RecurrenceRule()
   regla.month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-  regla.hour = 9
-  regla.minute = 30
+  regla.hour = 14
+  regla.minute = 00
   regla.second = 0
 
   regla.date = 1
